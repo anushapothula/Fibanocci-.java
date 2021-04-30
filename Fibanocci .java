c1f147f66945fa2e2e6c05c17b83cb64901f146f
anushapothula <83360113+anushapothula@users.noreply.github.com>
2021-04-30 14:30:48 +0530

    Update and rename README.md to Fibanocci .java

---
+++ Fibanocci .java
@@ -0,0 +1,19 @@
+  class Fibonacci{
+ public statuc void main(String[] args){
+ Scanner sc=new Scanner(System.in); 
+Syste.out.println("enter the range");
+ int range=sc.nextInt(); 
+int a=0; 
+int b=1;
+ intc=0; 
+System.out.println(a);
+ System.out.println(b); 
+for(int i=2;i&lt;=ran;i++){ 
+c=a+b; 
+if(c&lt;=range){ 
+System.out.println(c); 
+a=b; 
+b=c; 
+} 
+}
+}
